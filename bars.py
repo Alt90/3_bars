c7ce3f1aea25d5f0d7dab68d0111fcf37358bc0e
Alt13 <Alt90@rambler.ru>
2016-09-09 11:12:17 +0300

    Проведены правки по оформлению кода и описанию скрипта

--- bars.py
+++ bars.py
@@ -1,40 +1,50 @@
-import json, math
+import json
+import math
 
-def norm(coord_A, coord_B):
-    return math.sqrt((coord_A[0]-coord_B[0])*(coord_A[0]-coord_B[0])+
-        (coord_A[1]-coord_B[1])*(coord_A[1]-coord_B[1]))
+
+def get_distance(coord_A, coord_B):
+    return math.sqrt((coord_A[0] - coord_B[0]) * (coord_A[0] - coord_B[0]) +
+                     (coord_A[1] - coord_B[1])*(coord_A[1] - coord_B[1]))
+
 
 def load_data(filepath):
-    data = []
-    with open(filepath) as data_file:    
+    with open(filepath) as data_file:
         data = json.load(data_file)
     return(data)
 
 
-def get_biggest_bar(data):
-    return data[-1]['Cells']['Name']
+def get_biggest_bar(sorted_data):
+    return sorted_data[0]['Cells']['Name']
 
 
-def get_smallest_bar(data):
-    return data[0]['Cells']['Name']
+def get_smallest_bar(sorted_data):
+    return sorted_data[-1]['Cells']['Name']
 
 
 def get_closest_bar(data, longitude, latitude):
-    closest_bar = {'name': None, 'norm': 10000}
-    for bar in data:
+    coord_bar = data[0]['Cells']['geoData']['coordinates']
+    bar_norm = get_distance(coord_bar, [longitude, latitude])
+    closest_bar = {'name': data[0]['Cells']['Name'], 'norm': bar_norm}
+    for bar in data[1:]:
         coord_bar = bar['Cells']['geoData']['coordinates']
-        bar_norm = norm(coord_bar, [longitude, latitude])
+        bar_norm = get_distance(coord_bar, [longitude, latitude])
         if closest_bar['norm'] > bar_norm:
             closest_bar = {'name': bar['Cells']['Name'], 'norm': bar_norm}
     return closest_bar['name']
 
-if __name__ == '__main__':
+
+def get_sorted_data():
     data = load_data('source/bar.json')
-    data.sort(key=lambda x: x['Cells']['SeatsCount'])
-    print('The biggest bar is %s' % get_biggest_bar(data))
-    print('The smallest bar is %s' % get_smallest_bar(data))
+    data.sort(key=lambda x: x['Cells']['SeatsCount'], reverse=True)
+    return data
+
+
+if __name__ == '__main__':
+    sorted_data = get_sorted_data()
+    print('The biggest bar is %s' % get_biggest_bar(sorted_data))
+    print('The smallest bar is %s' % get_smallest_bar(sorted_data))
     longitude = float(input("Enter coordinates (long): "))
     latitude = float(input("Enter coordinates (lati): "))
-    print('The closest bar is %s' % get_closest_bar(data, longitude, latitude))
-    
-    
+    print('The closest bar is %s' % get_closest_bar(sorted_data,
+                                                    longitude,
+                                                    latitude))
